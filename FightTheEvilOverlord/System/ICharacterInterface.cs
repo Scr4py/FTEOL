6b5bda2deea517142c0a4cc07a6ab1cecce67048
Scr4py <kevinholst@gmx.net>
2015-01-28 15:07:19 +0100

    SpriteAnimation class added and finished

---
+++ FightTheEvilOverlord/System/ICharacterInterface.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FightTheEvilOverlord
+{
+    interface ICharacterInterface
+    {
+        private Transform transform;
+        protected int AttackPower;
+        public bool isDead = false;
+        public Tile tile;
+
+
+        public void GetDamage(GameObject gameObject)
+        {
+
+        }
+
+        public void Attack(GameObject target)
+        {
+
+        }
+
+        public void SetToStack()
+        {
+
+        }
+    }
+}
