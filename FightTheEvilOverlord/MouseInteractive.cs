f30c0ee40c137b20bf856f7ac7b1cd6c886c27b7
Scr4py <kevinholst@gmx.net>
2015-01-30 12:47:00 +0100

    Mousinteractive function

--- FightTheEvilOverlord/MouseInteractive.cs
+++ FightTheEvilOverlord/MouseInteractive.cs
@@ -2,12 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace FightTheEvilOverlord
 {
-    class MouseInteractive
+    class MouseInteractive : Component
     {
       public delegate void OnClickEventHandler();
       public static event OnClickEventHandler OnClick = delegate { };
+
+      Rectangle mouseRectangle;
+
+      public void start()
+      {
+          EventManager.OnUpdate += Update;
+      }
+
+      private void Update(GameTime gameTime)
+      {
+          
+      }
     }
 }
