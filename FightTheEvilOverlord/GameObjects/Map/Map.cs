31a6fc3b6d0d9bdd000e29f7c98f009d5edd6930
LucasZL <lucas_1996@live.de>
2015-02-02 21:58:47 +0100

    Just for being up-to-date on my PC

---
+++ FightTheEvilOverlord/GameObjects/Map/Map.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace FightTheEvilOverlord
+{
+    class Map : GameObject
+    {
+        int mapHeight = 13;
+        int mapWidth = 23;
+
+        Tiles[,] tilesArray;
+        public Map(int x, int y)
+        {
+            this.mapHeight = y;
+            this.mapWidth = x;
+            tilesArray = new Tiles[mapWidth, mapHeight];
+        }
+
+        public void generateTiles()
+        {
+            for (int y = 0; y < mapHeight; y++)
+            {
+                for (int x = 0; x < mapWidth; x++)
+                {
+                    tilesArray[x, y] = new Tiles();
+                    
+                    
+                       return new Tiles(Tiles.Type.)
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FightTheEvilOverlord/GameObjects/Map/Tiles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace FightTheEvilOverlord
+{
+    class Tiles
+    {
+        private enum Types { plaines, mountain, forest, village };
+
+        public Types type { get; set; }
+
+        public Tiles()
+        {
+            Random rnd = new Random();
+            int random = rnd.Next(2);
+        }
+        public Tiles(String village)
+        {
+
+        }
+    }
+}
