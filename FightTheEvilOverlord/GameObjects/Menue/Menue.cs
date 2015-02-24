bc19a9ebbd956c93e21b96430f4ca1d9d316d078
Scr4py <kevinholst@gmx.net>
2015-02-24 10:19:57 +0100

    Added new ParallaxLayer

--- FightTheEvilOverlord/GameObjects/Menue/Menue.cs
+++ FightTheEvilOverlord/GameObjects/Menue/Menue.cs
@@ -19,19 +19,12 @@ namespace FightTheEvilOverlord
         public Menue()
         {
             this.pm = new ParallaxManager();
-            //this.background = new Background(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\MenueBackground"));
-            //this.play = new Button(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_start"), GameState.GameStart, "parallax");
-            //this.play.GetComponent<Transform>().Position = new Vector2(700, 400);
-            //this.option = new Button(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_option"), GameState.Options, "parallax");
-            //this.option.GetComponent<Transform>().Position = new Vector2(700, 500);
-            //this.credits = new Button(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_credits"), GameState.Credits, "parallax");
-            //this.credits.GetComponent<Transform>().Position = new Vector2(700, 600);
-            //this.exit = new Button(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_exit"), GameState.Exit, "parallax");
-            //this.exit.GetComponent<Transform>().Position = new Vector2(700, 700);
+           
             this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\MenueBackground"), 1.0f, 0.35f, 0.0f, new Vector2(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 - 350, 0)));
             this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\OptionBackground"), 1.0f, 0.35f, 94.0f, new Vector2(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / 2 - 350, -GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height + 65)));
             this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\charac1"), 1.0f, 1.0f, 1.0f, new Vector2(0, 100)));
             this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\charac2"), 1.0f, 1.0f, 2.0f, new Vector2(GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 650, 100)));
+            //this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("wheat_tile"), 1.0f, 1.0f, 93.0f, new Vector2(800, 400)));
 
             this.play = new Button(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_start"), GameState.GameStart,"play", pm);
             this.play.GetComponent<Transform>().Position = new Vector2(700, 400);
@@ -49,13 +42,7 @@ namespace FightTheEvilOverlord
             this.exit.GetComponent<Transform>().Position = new Vector2(700, 700);
             this.pm.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_exit"), 1.0f, 1.0f, 3.0f, new Vector2(730, 700)));
 
-            //Utility.parallaxManager = new ParallaxManager();
-            //Utility.parallaxManager.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\MenueBackground"), 8.0f, 29.5f, 0.2f, 0.35f, new Vector2(700, 0)));
-            //Utility.parallaxManager.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_start"), 8.0f, 47.8f, 1.0f, 1.0f, new Vector2(700, 400)));
-            //Utility.parallaxManager.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_option"), 8.0f, 47.8f, 1.0f, 1.0f, new Vector2(700, 500)));
-            //Utility.parallaxManager.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_credits"), 8.0f, 47.8f, 1.0f, 1.0f, new Vector2(700, 600)));
-            //Utility.parallaxManager.Layers.Add(new ParallaxLayer(Utility.CurrentContent.Load<Texture2D>("MenuGraphics\\button_menu_exit"), 8.0f, 47.8f, 1.0f, 1.0f, new Vector2(700, 700)));
-
+       
             
             EventManager.OnUpdate += Update;
         }
